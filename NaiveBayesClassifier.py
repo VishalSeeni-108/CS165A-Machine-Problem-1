64a09e4f1638862e836fd3e9d39f50560739acb0
Viz108 <67886538+Viz108@users.noreply.github.com>
2024-05-09 16:33:10 -0700

    Organizing calculated conditional probs

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -8,10 +8,10 @@ def categoricalConditionalProb(category, descriptor):
         tempCounts = pd.Series(trainingData.loc[trainingData['weather_descriptions'].shift(periods=-1) == category, descriptor].value_counts()) #shift so that we are checking for FUTURE weather condition
         tempProbs = tempCounts.copy(deep = True)
         tempProbs = tempProbs.apply(lambda x : x / (tempCounts.sum()))
-        print("Category: " + category)
-        print(tempCounts)
-        print(tempProbs)
-        print('\n')
+        # print("Category: " + category)
+        # print(tempCounts)
+        # print(tempProbs)
+        # print('\n')
         return tempProbs
 
 
@@ -23,18 +23,23 @@ weatherDescriptionCounts = pd.Series(trainingData['weather_descriptions'].value_
 weatherDescriptionProb = weatherDescriptionCounts.copy(deep=True)
 weatherDescriptionProb = weatherDescriptionProb.apply(lambda x : x/(weatherDescriptionCounts.sum())) #p(Ck)
 
-print(weatherDescriptionCounts.index)
+#print(weatherDescriptionCounts.index)
 
 
 
 #humidity 
-print(pd.Series(trainingData['weather_descriptions'].unique()))
-print(categoricalConditionalProb('Clear', 'humidity'))
-print(categoricalConditionalProb('Sunny', 'humidity'))
-print(categoricalConditionalProb('Light freezing rain', 'precip'))
+# print(pd.Series(trainingData['weather_descriptions'].unique()))
+# print(categoricalConditionalProb('Clear', 'humidity'))
+# print(categoricalConditionalProb('Sunny', 'humidity'))
+# print(categoricalConditionalProb('Light freezing rain', 'precip'))
 
 #for a given descriptor, we need to apply categoricalConditiaonalProb with all categories, then store it in an associated descriptor datafram
-humidityConditionalProbs = pd.DataFrame()
-humidityConditionalProbs.concat(weatherDescriptionCounts.index.apply(lambda x : categoricalConditionalProb(x, 'humidity')))
 
+#pd.concat(humidityConditionalProbs, (((weatherDescriptionCounts.index).to_series()).apply(lambda x : categoricalConditionalProb(x, 'humidity'))).to_frame().T)
+humidityConditionalProbs = ((weatherDescriptionCounts.index).to_series()).apply(lambda x : categoricalConditionalProb(x, 'humidity'))
+cloudcoverConditionalProbs = ((weatherDescriptionCounts.index).to_series()).apply(lambda x : categoricalConditionalProb(x, 'cloudcover'))
+precipitationConditionalProbs = ((weatherDescriptionCounts.index).to_series()).apply(lambda x : categoricalConditionalProb(x, 'precip'))
+print(humidityConditionalProbs)
+print(cloudcoverConditionalProbs)
+print(precipitationConditionalProbs)
 
